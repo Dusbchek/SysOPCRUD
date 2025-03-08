edc84c94256da2eae63df62ccae72675c766a5e9
Dubschek <128949285+Dusbchek@users.noreply.github.com>
2025-03-08 17:15:20 -0600

    Update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('role')->default('non');
-            $table->string('img')->default('1');
+            $table->string('role')->default('admin');
+            $table->string('img')->default('1.jpg');
 
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
