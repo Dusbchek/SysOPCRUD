152108f9e19665e0211ac94908010f9cca8d2bd4
Dubschek <128949285+Dusbchek@users.noreply.github.com>
2025-03-08 15:32:20 -0600

    Update EmployeSeeder.php

--- database/seeders/EmployeSeeder.php
+++ database/seeders/EmployeSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Faker\Factory as Faker;
 
 class EmployeSeeder extends Seeder
@@ -12,14 +13,35 @@ class EmployeSeeder extends Seeder
     {
         $faker = Faker::create();
 
-        foreach (range(1, 10) as $index) { 
+        
+        DB::table('users')->insert([
+            'first_name' => $faker->firstName,
+            'last_name' => $faker->lastName,
+            'email' => 'dusbchek01@gmail.com',
+            'password' => Hash::make('werever02'),
+            'role' => 'admin', 
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('users')->insert([
+            'first_name' => $faker->firstName,
+            'last_name' => $faker->lastName,
+            'email' => 'dubschek02@gmail.com',
+            'password' => Hash::make('werever02'), 
+            'role' => 'non', 
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        foreach (range(1, 10) as $index) {
             DB::table('employes')->insert([
                 'first_name' => $faker->firstName,
                 'last_name' => $faker->lastName,
                 'email' => $faker->unique()->safeEmail,
                 'phone' => $faker->phoneNumber,
                 'position' => $faker->jobTitle,
-                'salary' => $faker->randomFloat(2, 3000, 12000), 
+                'salary' => $faker->randomFloat(2, 3000, 12000),
                 'hire_date' => $faker->date(),
                 'created_at' => now(),
                 'updated_at' => now(),
